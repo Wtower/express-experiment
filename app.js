1604d9767892fc56cbd0cfbb94995f82508afd3b
Wtower <gkarak@9-dev.com>
2015-12-24 12:42:41 +0200

    Added www

--- app.js
+++ app.js
@@ -4,11 +4,3 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
-console.log('Ok');
